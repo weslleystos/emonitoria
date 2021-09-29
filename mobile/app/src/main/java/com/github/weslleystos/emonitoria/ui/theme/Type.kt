9e373f60cea7e3a7e620245c607b26ecdec367b8
Weslley Campos <weslley_campos@icloud.com>
2021-09-28 21:19:44 -0300

    Adding custom fonts.

--- mobile/app/src/main/java/com/github/weslleystos/emonitoria/ui/theme/Type.kt
+++ mobile/app/src/main/java/com/github/weslleystos/emonitoria/ui/theme/Type.kt
@@ -1,28 +1,32 @@
 package com.github.weslleystos.emonitoria.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.github.weslleystos.emonitoria.R
+
+
+val appFontFamily = FontFamily(
+    Font(R.font.nunito_regular, weight = FontWeight.Normal),
+    Font(R.font.nunito_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.nunito_bold, weight = FontWeight.Bold),
+    Font(R.font.nunito_black, weight = FontWeight.Black),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-        body1 = TextStyle(
-                fontFamily = FontFamily.Default,
-                fontWeight = FontWeight.Normal,
-                fontSize = 16.sp
-        )
-        /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    h1 = Typography().h1.copy(fontFamily = appFontFamily),
+    h2 = Typography().h2.copy(fontFamily = appFontFamily),
+    h3 = Typography().h3.copy(fontFamily = appFontFamily),
+    h4 = Typography().h4.copy(fontFamily = appFontFamily),
+    h5 = Typography().h5.copy(fontFamily = appFontFamily),
+    h6 = Typography().h6.copy(fontFamily = appFontFamily),
+    subtitle1 = Typography().subtitle1.copy(fontFamily = appFontFamily),
+    subtitle2 = Typography().subtitle2.copy(fontFamily = appFontFamily),
+    body1 = Typography().body1.copy(fontFamily = appFontFamily),
+    body2 = Typography().body2.copy(fontFamily = appFontFamily),
+    button = Typography().button.copy(fontFamily = appFontFamily),
+    caption = Typography().caption.copy(fontFamily = appFontFamily),
+    overline = Typography().overline.copy(fontFamily = appFontFamily)
 )
