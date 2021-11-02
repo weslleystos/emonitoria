8e6afb3a9e9ec2a6e9d39ab46c97d93f5c775ef7
Weslley Campos <weslley_campos@icloud.com>
2021-11-02 17:40:49 -0300

    Creation of the base project.

---
+++ mobile/app/src/main/java/com/github/weslleystos/emonitoria/shared/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.weslleystos.emonitoria.shared.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.weslleystos.emonitoria.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
