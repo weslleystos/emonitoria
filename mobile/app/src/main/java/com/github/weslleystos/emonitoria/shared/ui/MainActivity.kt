99c7d96fdcda4b764e16239b8a62e7e603b442eb
Weslley Campos <weslley_campos@icloud.com>
2021-11-08 20:37:28 -0300

    Creation of the signIn screen.

--- mobile/app/src/main/java/com/github/weslleystos/emonitoria/shared/ui/MainActivity.kt
+++ mobile/app/src/main/java/com/github/weslleystos/emonitoria/shared/ui/MainActivity.kt
@@ -2,11 +2,14 @@ package com.github.weslleystos.emonitoria.shared.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.github.weslleystos.emonitoria.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
     }
 }
