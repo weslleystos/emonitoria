beeba2c6a0c2dea349e30792d7ce128117b732b3
Weslley Campos <weslley.campos@icloud.com>
2021-12-16 11:02:03 -0300

    Refactoring Resource class.

--- android/Domain/src/main/java/com/github/weslleystos/emonitoria/domain/shared/model/Resource.kt
+++ android/Domain/src/main/java/com/github/weslleystos/emonitoria/domain/shared/model/Resource.kt
@@ -1,19 +1,23 @@
 package com.github.weslleystos.emonitoria.domain.shared.model
 
+import com.github.weslleystos.emonitoria.domain.shared.model.State.*
+
 class Resource<T>(val state: State, val data: T? = null, val throwable: Throwable? = null) {
     companion object {
-        fun <T> loading(): Resource<T> = Resource(State.LOADING)
+        fun <T> init(): Resource<T> = Resource(INIT)
 
-        fun <T> loaded(): Resource<T> = Resource(State.LOADED)
+        fun <T> loading(): Resource<T> = Resource(LOADING)
 
-        fun <T> success(data: T): Resource<T> = Resource(State.SUCCESS, data)
+        fun <T> loaded(): Resource<T> = Resource(LOADED)
 
-        fun <T> failure(throwable: Throwable?): Resource<T> =
-            Resource(State.FAILURE, null, throwable)
+        fun <T> success(data: T): Resource<T> = Resource(SUCCESS, data)
+
+        fun <T> failure(throwable: Throwable?): Resource<T> = Resource(FAILURE, null, throwable)
     }
 }
 
 enum class State {
+    INIT,
     SUCCESS,
     FAILURE,
     LOADING,
@@ -21,21 +25,21 @@ enum class State {
 }
 
 inline fun <T> Resource<T>.onLoading(action: () -> Unit): Resource<T> {
-    if (state == State.LOADING) action()
+    if (state == LOADING) action()
     return this
 }
 
 inline fun <T> Resource<T>.onSuccess(action: (T) -> Unit): Resource<T> {
-    if (state == State.SUCCESS) data?.let(action)
+    if (state == SUCCESS) data?.let(action)
     return this
 }
 
 inline fun <T> Resource<T>.onFailure(action: (Throwable) -> Unit): Resource<T> {
-    if (state == State.FAILURE) throwable?.let(action)
+    if (state == FAILURE) throwable?.let(action)
     return this
 }
 
 inline fun <T> Resource<T>.onLoaded(action: () -> Unit): Resource<T> {
-    if (state == State.LOADED) action()
+    if (state == LOADED) action()
     return this
 }
