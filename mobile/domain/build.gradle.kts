cf2eb78d4cd198b1e22c5986f9a88234871c269e
weslleystos <weslley.campos@icloud.com>
2022-06-11 22:10:43 -0300

    Created domain architecture layer.

---
+++ mobile/domain/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("kotlin")
+    kotlin("kapt")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+dependencies { }
--- mobile/settings.gradle.kts
+++ mobile/settings.gradle.kts
@@ -14,3 +14,4 @@ dependencyResolutionManagement {
 }
 rootProject.name = "E-Monitoria"
 include(":app")
+include(":domain")
